0d37892446e682a155fe40cf580ffb16b4f50cdf
Thegodlessman <luischang16@hotmail.com>
2025-05-09 20:17:32 -0400

    Fix: Reincorporacion de la carpeta Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <UsersTable />
+    </div>
+</template>
+
+<script>
+import UsersTable from './components/usersTable.vue';
+
+export default {
+  name: 'App',
+  components: {
+    UsersTable
+  }
+};
+</script>
+
+<style>
+/* Estilos globales */
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
